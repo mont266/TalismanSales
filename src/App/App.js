73b3ca9a03b952a85ba0711f58a4108df83a2836
Jukka Koivu <skuuppi11@hotmail.com>
2021-11-21 15:21:51 +0200

    First commit
    Includes login page template and page structures

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navigator from '../Pages/Navigation';
+
+function App() {
+  return (
+    <Navigator />
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { render } from "react-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./index.css"
+import Login from "./Pages/Authentication/Login"
+import Home from "./Pages/Home/index"
+import reportWebVitals from "./reportWebVitals"
+
+const rootElement = document.getElementById("root")
+
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="home" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
