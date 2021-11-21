69c388ab9678c96a9ed269d7da6ce818e3e006a2
Jukka Koivu <skuuppi11@hotmail.com>
2021-11-21 16:10:03 +0200

    First calculator started
    full website calculator started

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './Home.css';
+import React, { useState } from "react"
+import Box from "@mui/material/Box"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+import FormControl from "@mui/material/FormControl"
+import Select from "@mui/material/Select"
+import TextField from "@mui/material/TextField"
+import logo from "./logo.svg"
+import "./Home.css"
 
 function Home() {
+  const [website, setWebsite] = useState("")
+
+  const handleChange = (event) => {
+    setWebsite(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn About epicness
-        </a>
-      </header>
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Box sx={{ minWidth: 220 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">
+            Website/Service Type
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={website}
+            label="Website/Service Type"
+            onChange={handleChange}>
+            <MenuItem value={10}>Full Website</MenuItem>
+            <MenuItem value={20}>Landing Page</MenuItem>
+            <MenuItem value={30}>Accommodation Website</MenuItem>
+            <MenuItem value={40}>Social Media Page</MenuItem>
+          </Select>
+          <TextField
+            required
+            margin="normal"
+            fullWidth
+            id="Your Name"
+            label="Your Name"
+            name="name"
+            autoFocus
+            onChange={() => {}}
+          />
+          <TextField
+            required
+            margin="normal"
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoFocus
+            onChange={() => {}}
+          />
+        <TextField
+            required
+            margin="normal"
+            fullWidth
+            id="pagesQT"
+            label="How Many Web Pages?"
+            name="pagesQty"
+            autoFocus
+            onChange={() => {}}
+          />
+        </FormControl>
+      </Box>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
