783b49875b27c5e30350d15f493a640a0c263d7a
Jukka Koivu <skuuppi11@hotmail.com>
2021-11-21 17:03:05 +0200

    Calculator
    Tabs for different calculators

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,55 +5,43 @@ import MenuItem from "@mui/material/MenuItem"
 import FormControl from "@mui/material/FormControl"
 import Select from "@mui/material/Select"
 import TextField from "@mui/material/TextField"
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import logo from "./logo.svg"
 import "./Home.css"
 
 function Home() {
   const [website, setWebsite] = useState("")
+  const [value, setValue] = useState('one')
 
   const handleChange = (event) => {
     setWebsite(event.target.value)
   }
 
+  const handleTabChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
       <img src={logo} className="App-logo" alt="logo" />
+      <Box sx={{ width: '100%', marginBottom: 10 }}>
+      <Tabs
+        centered
+        value={value}
+        onChange={handleTabChange}
+        textColor="secondary"
+        indicatorColor="secondary"
+        aria-label="secondary tabs example"
+      >
+        <Tab value="one" label="Full Website" />
+        <Tab value="two" label="Landing Page" />
+        <Tab value="three" label="Accommodation Website" />
+        <Tab value="three" label="Social Media Page" />
+      </Tabs>
+    </Box>
       <Box sx={{ minWidth: 220 }}>
         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">
-            Website/Service Type
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={website}
-            label="Website/Service Type"
-            onChange={handleChange}>
-            <MenuItem value={10}>Full Website</MenuItem>
-            <MenuItem value={20}>Landing Page</MenuItem>
-            <MenuItem value={30}>Accommodation Website</MenuItem>
-            <MenuItem value={40}>Social Media Page</MenuItem>
-          </Select>
-          <TextField
-            required
-            margin="normal"
-            fullWidth
-            id="Your Name"
-            label="Your Name"
-            name="name"
-            autoFocus
-            onChange={() => {}}
-          />
-          <TextField
-            required
-            margin="normal"
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoFocus
-            onChange={() => {}}
-          />
         <TextField
             required
             margin="normal"
