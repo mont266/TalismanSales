1086281a1281a4093a92f35d670f0b811460424f
Jukka Koivu <skuuppi11@hotmail.com>
2021-11-22 22:56:51 +0200

    Mobile tabs
    Tabs should work better now on mobile screens

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,44 +5,53 @@ import MenuItem from "@mui/material/MenuItem"
 import FormControl from "@mui/material/FormControl"
 import Select from "@mui/material/Select"
 import TextField from "@mui/material/TextField"
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import Tabs from "@mui/material/Tabs"
+import Tab from "@mui/material/Tab"
+
+import useWindowDimensions from "../../Hooks/ScreenDimensions"
+import MobileTabs from "../../Components/MobileTabs"
+
 import logo from "./logo.svg"
 import "./Home.css"
 
 function Home() {
+  const { height, width } = useWindowDimensions()
   const [website, setWebsite] = useState("")
-  const [value, setValue] = useState('one')
+  const [value, setValue] = useState("one")
 
   const handleChange = (event) => {
     setWebsite(event.target.value)
   }
 
   const handleTabChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    setValue(newValue)
+  }
 
   return (
     <div>
       <img src={logo} className="App-logo" alt="logo" />
-      <Box sx={{ width: '100%', marginBottom: 10 }}>
-      <Tabs
-        centered
-        value={value}
-        onChange={handleTabChange}
-        textColor="secondary"
-        indicatorColor="secondary"
-        aria-label="secondary tabs example"
-      >
-        <Tab value="one" label="Full Website" />
-        <Tab value="two" label="Landing Page" />
-        <Tab value="three" label="Accommodation Website" />
-        <Tab value="three" label="Social Media Page" />
-      </Tabs>
-    </Box>
+      {width < 500 ? (
+        <MobileTabs />
+      ) : (
+        <Box sx={{ width: "100%", marginBottom: 10 }}>
+          <Tabs
+            centered
+            value={value}
+            onChange={handleTabChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="secondary tabs example">
+            <Tab value="one" label="Full Website" />
+            <Tab value="two" label="Landing Page" />
+            <Tab value="three" label="Accommodation Website" />
+            <Tab value="three" label="Social Media Page" />
+          </Tabs>
+        </Box>
+      )}
+
       <Box sx={{ minWidth: 220 }}>
         <FormControl fullWidth>
-        <TextField
+          <TextField
             required
             margin="normal"
             fullWidth
